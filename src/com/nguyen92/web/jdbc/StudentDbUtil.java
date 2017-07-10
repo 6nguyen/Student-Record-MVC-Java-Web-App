0241f004a1118f010bf4ba7eaac5941d5951cd24
George Nguyen <g.nguyen92@yahoo.com>
2017-07-10 12:36:19 -0700

    StudentDbUtil: Add method to list Student Objects

--- src/com/nguyen92/web/jdbc/StudentDbUtil.java
+++ src/com/nguyen92/web/jdbc/StudentDbUtil.java
@@ -1,5 +1,8 @@
 package com.nguyen92.web.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 public class StudentDbUtil {
@@ -7,10 +10,19 @@ public class StudentDbUtil {
 	// Step 1:  Set up reference to a DataSource
 	private DataSource dataSource;
 	
-	// Step 2:  Set up constructor
+	// Step 2:  Set up constructor, used by Servlet to pass db as parameter	
 	public StudentDbUtil(DataSource dataSource){
 		this.dataSource = dataSource;
 	}
+	
+	
+	// Step 3:  Write method to list the Student Objects
+	public List<Student> getStudents() throws Exception {
+		List<Student> students = new ArrayList<>();
+		
+		return students;
+	}
+	
 	
 	
 }
