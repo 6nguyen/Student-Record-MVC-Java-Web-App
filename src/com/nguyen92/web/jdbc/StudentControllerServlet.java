36177fb236fd6f03448b51c7b426235ff8e0034c
George Nguyen <g.nguyen92@yahoo.com>
2017-07-10 12:58:57 -0700

    StudentControllerServlet: Add ref to Java EE Resource Injection

--- src/com/nguyen92/web/jdbc/StudentControllerServlet.java
+++ src/com/nguyen92/web/jdbc/StudentControllerServlet.java
@@ -1,11 +1,14 @@
 package com.nguyen92.web.jdbc;
 
 import java.io.IOException;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class StudentControllerServlet
@@ -14,11 +17,25 @@ import javax.servlet.http.HttpServletResponse;
 public class StudentControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	
+	// Step 1: Set up reference to DB Util class
+	private StudentDbUtil studentDbUtil;
+		
+	
+	// Step 2:  Set up ref to Java EE Resource Injection
+	// Define datasource/connection pool for Resource Injection
+	// resource name found in META-INF>context.xml>Resource name
+	@Resource(name="jdbc/webStudentTracker")
+	private DataSource dataSource;
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+
+	
 	}
 
 }
